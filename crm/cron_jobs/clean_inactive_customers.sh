6b0908786366878e919b4b436197f9592e1a6042
elainees-devs <elainees.devs@gmail.com>
2025-08-27 10:36:57 +0300

    Update clean_inactive_customers.sh to use count() before deletion

--- crm/cron_jobs/clean_inactive_customers.sh
+++ crm/cron_jobs/clean_inactive_customers.sh
@@ -11,8 +11,10 @@ DELETED_COUNT=$(python3 manage.py shell -c "
 from crm.models import Customer
 from datetime import datetime, timedelta
 one_year_ago = datetime.now() - timedelta(days=365)
-deleted, _ = Customer.objects.filter(order__date__lt=one_year_ago).delete()
-print(deleted)
+inactive_customers = Customer.objects.filter(order__date__lt=one_year_ago)
+count = inactive_customers.count()
+inactive_customers.delete()
+print(count)
 ")
 
 # Log the number of deleted customers
