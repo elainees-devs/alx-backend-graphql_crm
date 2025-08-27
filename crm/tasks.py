aefef5f609dcd4b875597fe0f08bcc9a2e734c74
elainees-devs <elainees.devs@gmail.com>
2025-08-27 11:42:50 +0300

    Add imports datetime and requests

--- crm/tasks.py
+++ crm/tasks.py
@@ -1,5 +1,6 @@
 # crm/tasks.py
-import datetime
+from datetime import datetime
+import requests
 from celery import shared_task
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
