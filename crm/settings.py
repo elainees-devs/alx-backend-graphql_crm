a88310ef74cfdd8b4e132ec1d969fd58b8b6cd45
elainees-devs <elainees.devs@gmail.com>
2025-08-27 10:58:25 +0300

    Add django_crontab to INSTALLED_APPS and configure heartbeat cron job

--- crm/settings.py
+++ crm/settings.py
@@ -1,2 +1,13 @@
 # crm/settings.py
 from alx_backend_graphql.settings import *
+
+# Ensure INSTALLED_APPS includes 'django_crontab'
+INSTALLED_APPS += [
+    'django_crontab',
+]
+
+# Add CRONJOBS
+CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+]
+
